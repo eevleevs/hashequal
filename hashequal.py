08773d771a7b202f243d18a38ca1fd9380393ad9
Giulio Malventi <giulio.malventi@gmail.com>
2020-03-19 09:27:30 +0100

    Full rewrite

--- hashequal.py
+++ hashequal.py
@@ -1,81 +1,54 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 
 # a Python module to make quasi interactive math sheets
 # https://github.com/eevleevs/hashequal
 
-# options
-align_results = False  # if True aligns all results to the rightmost #=
-
-# import
 import datetime
 import inspect
-import json
-import os
-import random
 import re
+import shutil
 
-# init variables
-f = inspect.getframeinfo(inspect.stack()[-1][0]).filename  # caller script name
-if f == '<stdin>':
+# load caller script
+caller_filename = inspect.getframeinfo(inspect.stack()[-1][0]).filename
+if caller_filename == '<stdin>':
     raise ImportError('hashequal cannot be imported in interactive mode')
-c = open(f).read().split('\n')  # caller script content lines
-p = 0  # alignment counter
-s = ''  # aux string
-t = 'hashequal_temp'  # temporary file name
-h = 'hashequal_data' + str(random.randrange(1e10))  # data variable name (randomised)
-e = r'(\s*([\w\d]+)\s*?=?.*?)#=(.*)'  # search pattern
+s1 = open(caller_filename, encoding='utf-8').read()
 
-# rewrite caller script to save results of operations marked with #= and save to temporary file
-ic = 1
-for i in c:
-    if 'import hashequal' in i:  # prevent another call of import hashequal
-        s += i.replace('import hashequal', 'import chardet; import json; ' + h + ' = []') + '\n'  # import other modules and init data container
-    else:
-        m = re.match(e, i)  # match #= lines
-        if m:
-            s += m.group(1) + '; ' + h + '.append(\'' + str(ic) + ':\' + str(' + m.group(2) + ').decode(chardet.detect(str(' + m.group(2) + '))[\'encoding\']))\n'  # insert code for saving to temporary variable
-            if (len(m.group(1)) > p):
-                p = len(m.group(1))  # adjust alignment counter
-        else:
-            s += i + '\n'
-    ic += 1
-s += 'open(\'' + t + '\', \'w\').write(json.dumps(' + h + '))'  # insert code for writing results
-open(t, 'w').write(s)  # write modified script
+# assign a progressive number to every #=, e.g. "#=/number/"
+counter = 0
+for line in re.findall(r'^.*?#=.*$', s1, flags=re.MULTILINE):
+    s1 = s1.replace(line, re.sub(r'#=.*?(#?.*)', r'#=/' + str(counter) + r'/\1', line), 1)
+    counter += 1
 
-# run modified script, overwrites self with results
-if not os.system('python ' + t):
+# remove line continuations
+s2 = s1.replace('\\\n', '')
 
-    # load results and rewrite original script to include them as comments
-    d = {}
-    for i in json.loads(open(t).read()):
-        i = i.split(':', 1)
-        try:
-            d[i[0]].append(i[1])
-        except KeyError:
-            d[i[0]] = [i[1]]
-    s = ''
-    if align_results:
-        p -= 1
-    else:
-        p = 0
-    ic = 1
-    for i in c:
-        if str(ic) in d.keys():
-            m = re.match(e, i) 
-            s += m.group(1).rstrip().ljust(p) + ' #= ' 
-            while d[str(ic)]:
-                s += (d[str(ic)].pop(0).encode('utf8') + b', ').decode('utf8')  # insert result
-            s = s[:-2]  # remove last comma and space
-            if '#' in m.group(3):
-                s += '  # ' + m.group(3).split('#', 1)[1].strip()  # insert eventual comments after result
-        elif 'import hashequal' in i:
-            s += i.split('#')[0].rstrip() + '  # run ' + str(datetime.datetime.utcnow()).split('.')[0] + ' UTC'  # insert run time on import hashequal line
-        else:
-            s += i          
-        s += '\n'
-        ic += 1
-    open(f, 'w').write(s[:-1])  # overwrite original script with annotated file
+# substitute "import hashequal" with "pass"
+s2 = re.sub(r'^(\s+).*?import hashequal.*$', r'\1pass', s2, flags=re.MULTILINE)
 
-# remove temporary file and prevent execution of orginal script
-os.remove(t)
+# prepend "hashequal_data[number] = " to every line with a "#=/number/"
+s2 = re.sub(r'(\s*)(.*?)#=\/(\d+)\/(.*)', r'\1hashequal_data[\3] = \2', s2)
+
+# execute s2 retrieving hashequal_data
+hashequal_data = {}
+exec(s2, {'hashequal_data':hashequal_data, '__name__':'__main__'})
+
+# substitute results in s1
+for key,value in hashequal_data.items():
+    s1 = re.sub(r'#=\/' + str(key) + r'\/.*?(#.*)?$', r'#= ' + str(value) + r'  \1',
+        s1, flags=re.MULTILINE)
+
+# clear keys that were not evaluated
+s1 = re.sub(r'#=\/.*?(#.*)?$', r'#= n/a  \1', s1, flags=re.MULTILINE)
+s1 = re.sub(r'#=  $', r'#= n/a', s1, flags=re.MULTILINE)
+
+# mark run time
+s1 = re.sub(r'(.*?import hashequal).*', r'\1  # run '
+    + str(datetime.datetime.utcnow()).split('.')[0] + ' UTC', s1)
+
+# overwrite caller script, possibly making a backup
+shutil.copyfile(caller_filename, caller_filename + '~')
+open(caller_filename, 'w', encoding='utf-8').write(s1)
+
+# stop execution
 exit()
